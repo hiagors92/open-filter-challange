b6ae5e14e33ba9c45a830fc682d9cebc31002dc5
hiagors92 <hiagors92@gmail.com>
2025-07-15 04:42:32 -0300

    fix

--- openfilter/examples/hello-world/test/integration/tests/test_pipeline.py
+++ openfilter/examples/hello-world/test/integration/tests/test_pipeline.py
@@ -1,4 +1,5 @@
 from pytest_bdd import scenarios
 
 scenarios('../features/pipeline_success.feature')
-scenarios('../features/pipeline_errors.feature')
+scenarios('../features/pipeline_failure.feature')
+scenarios('../features/pipeline_performance.feature')
